78a4f13b1202615930dc9841a493dbf07ce674c5
David Gutierrez <dcgutierrez93@yahoo.com>
2017-12-03 15:31:44 -0600

    Adding to composition

---
+++ Resolution.java
@@ -0,0 +1,18 @@
+public class Resolution {
+    private int width;
+    private int height;
+
+    // Consturctor
+    public Resolution(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    // Getters
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+}
