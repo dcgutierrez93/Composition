2b37c650a895697138afc42b81fc1b5e0f1c3ecb
dcgutierrez93 <dcgutierrez93@yahoo.com>
2017-12-04 10:17:45 -0600

    added dimensions

---
+++ Dimensions.java
@@ -0,0 +1,23 @@
+public class Dimenstions {
+    private int width;
+    private int height;
+    private int depth;
+
+    // Consturctor
+    public Dimensions(int width, int height, int depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    // Getters
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public int getDepth() {
+        return depth;
+    }
+}
