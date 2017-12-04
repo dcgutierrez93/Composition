11fe32faec02ac360d20eeac653fa39a4891b501
dcgutierrez93 <dcgutierrez93@yahoo.com>
2017-12-04 10:38:02 -0600

    added to main

---
+++ PC.java
@@ -0,0 +1,23 @@
+public class PC {
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    // Consturctor
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    // Getters
+    public Case getTheCase() {
+        return theCase;
+    }
+    public Monitor getMonitor() {
+        return monitor;
+    }
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
+}
